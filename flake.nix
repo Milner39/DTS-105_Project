728c4aa25f13f0fcd1b3085c52792bcdc2cae24e
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-01-31 14:32:36 +0000

    feat: add flake to pin UV version

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system;
+      config.allowUnfree = true;
+    };
+  in
+  {
+    # nix develop
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        uv
+        python315
+      ];
+
+      UV_PYTHON_DOWNLOADS = "never";
+    };
+  };
+}
